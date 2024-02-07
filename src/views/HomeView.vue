760b56a2740c693d97b8fcd31c770325982635b5
Kanneli <joodhahmednaseer@gmail.com>
2024-02-07 15:22:56 +0500

    adapted the design to be a bit more responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,19 @@ console.log("test")
   <main>
     <div id="home">
       <div id="coming-soon" class="shadow-amber-800" style="box-shadow: inset 0 2px 16px 0 rgba(212, 164, 19, 0.435);">
-        <h1>Hello world</h1>
-        <h2>by Joodh Ahmed Naseer</h2>
-        <div class="flex justify-center ml-16 my-9 md:mt-16 md:mb-24">
-          <img class="w-96" src="@/assets/Hello World.png" alt="">
+        <div class="md:flex md:flex-col md:justify-center p-8">
+          <h1>Hello world</h1>
+          <h2>Joodh Ahmed Naseer</h2>
+          <p class="w-96 pt-4">
+            This is supposed to be a short intro of myself, that I will finish one day.
+          </p>
         </div>
-        <h2>~Will finish this oneday~</h2>
+        
+        <div class="md:flex md:flex-col md:justify-center">
+          <div class="flex justify-center ml-16 my-9 md:mt-16 md:mb-24">
+            <img class="w-96" src="@/assets/Hello World.png" alt="">
+          </div>
+        </div>
       </div>
     </div>
   </main>
